540130bc955aef23cd7b016b4c94b4d4c2f05bd3
ashirwadexe <ashirwadbappy@gmail.com>
2026-08-20 18:06:10 +0530

    learn basic, output, input, data types, variables, keywords and identifers, type conversion, literals

--- day1.py
+++ day1.py
@@ -60,3 +60,132 @@ print({1,2,3,4,5});
 
 # 9. Dictionary
 print({'name': 'Ashirwad', 'Age': 23, 'Weight': 80});
+
+
+# type() Function
+print(type('Hello'))
+print(type(1))
+print(
+    type(1+7j),
+    type(True)
+)
+
+# ---------------------------------------
+# -----------VARIABLES-------------------
+# ---------------------------------------
+
+# variables are containers used to store data for future use
+# declaration of variable in python
+
+name = "Ashirwad"
+print('name is: ', name)
+
+a = 3
+b = 5
+print('add: ', a+b)
+
+# DYNAMIC TYPING - yaha ham variable ka type nhi define krte direct variable likh denge py ka interpreter khud find kr lega ki inte hai ya float ya string
+result = "string"
+
+# STATIC TYPING - yaha datatype define krna hoga
+# int a = 5;
+
+# DYNAMIC BINDING - isme varibale ka data type fix nhi hoti aage ja ke change ho sakti hai
+name = "Bappy"
+print(name)
+name = 33.55
+print(name)
+
+# Smart way to define multiple variables
+a=2
+b='name'
+c=777.987
+print(a,b,c)
+
+a,b,c, = 2, 'name', 777.987
+print(a,b,c)
+
+
+# ------------------------------------------------------
+# -----------KEYWORDS & IDENTIFIERS-------------------
+# ------------------------------------------------------
+
+# KEYWORDS - These are reserved words for python, dont use them as variables (33 keywords in Py)
+# Compilation - converting the english code into machine language(0/1)
+# Compilor - converts the whole code in one go - FAST 
+# Interpretor - converts the code line by line - SLOW 
+
+# Identifiers - a name created by the programmer
+# Rules to create a identifier
+# 1. not start with digit
+# 2. only underscore(_) special character can be used in naming
+# 3. identifiers cannot be keywords
+
+
+# ------------------------------------------------------
+# -----------USER INPUT---------------------------------
+# ------------------------------------------------------
+
+name = input('Enter name: ')
+print('Your Name is : ',name)
+
+# IMPORTANT: input by default input string data type me leta hai kyoki ye ek unversal data form h isme baki sare types store ho sakte hai 
+# add 2 numbers
+fnum = input("enter 1st no: ");
+snum = input("enter 2nd no: ");
+
+sum = int(fnum)+ int(snum);
+print('sum is: ', sum)
+
+
+# ------------------------------------------------------
+# -----------TYPE CONVERSION----------------------------
+# ------------------------------------------------------
+
+# Implicit - interpretor khud kr deta hai programmer ko kuch nhi krna
+# Explicit - programmer type conversion krega 
+# str to int (comples cant be changed to integer)
+# type(int('4'))
+# type(int(4.5))
+
+# # int to str
+# print(str(5))
+# print(str(5.7665))
+
+# ------------------------------------------------------
+# -----------LITERALS----------------------------
+# ------------------------------------------------------
+
+# It is the raw value stored in a variable
+a = 0b1010 #Binary Literal - 0b - bata raha h ki aage ki value binary me hia
+b = 100 #Decimal Literal
+c = 0o310 #Octal Literal
+d = 0x12c # hexadecimal Literal
+
+# Float Loteral
+float_1 = 10.5
+float_2 = 1.5e3 #1.5*10^3 - very big number
+float_3 = .5e-3 #1.5*1^-3 - very small number
+
+# Complex Literal
+x = 3.14j
+print(x.imag, x.real)
+
+# String Literal
+string = "This is Python."
+strings = 'This is Python.'
+char = "C"
+multiline_string = """This is a multiline string with a more than one line code."""
+unicode = u"\U0001F600\U0001F606\U0001F923"
+raw_str = r"raw \n string"
+
+print(string)
+print(strings)
+print(char)
+print(multiline_string)
+print(unicode)
+print(raw_str)
+
+# None Literal
+ab = None
+print(ab)
