9f05a1c9281198837b373a24c6bf72c86434bcec
ashirwadexe <ashirwadbappy@gmail.com>
2026-09-08 21:47:39 +0530

    day9 -> Abstraction

--- day9.py
+++ day9.py
@@ -395,3 +395,27 @@
 #     [1,2,3] + [4,5] = [1,2,3,4,5]
 
 
+# # =====================================================
+# # ================== ABSTRACTION ======================
+# # =====================================================
+
+# # Abstraction - means hidden
+
+# from abc import ABC, abstractmethod
+# class BankApp(ABC):
+#     def database(self):
+#         print('connected to database')
+
+#     @abstractmethod
+#     def security(self):
+#         pass
+
+# class MobileApp(BankApp):
+#     def mobile_login(self):
+#         print('login into mobile')
+
+#     def security(self):
+#         print('mobile security')
+
+# mob = MobileApp()
+# mob.mobile_login()
